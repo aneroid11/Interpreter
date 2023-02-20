920f9f6c0d7d0a2c42df7bb5040075fa0ce4d4e0
lucky <>
2023-02-20 16:37:58 +0300

    dummy lexer

--- lab2/main.py
+++ lab2/main.py
@@ -1,7 +1,11 @@
+class Lexer:
+    def __init__(self, file_name: str):
+        print(file_name)
+
+
 def main():
-    print("hello world")
+    lexer = Lexer("program.cmm")
 
 
 if __name__ == "__main__":
     main()
-    
