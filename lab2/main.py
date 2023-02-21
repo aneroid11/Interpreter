fbf8c9aee493a4bbe4a21304fc9bc6a80160e4a2
lucky <>
2023-02-21 16:27:53 +0300

    special symbols

--- lab2/main.py
+++ lab2/main.py
@@ -5,8 +5,28 @@ class Lexer:
     # types of tokens
     INT, DOUBLE, BOOL, STRING, WHILE, FOR, IF, ELSE, SWITCH, CASE, BREAK, DEFAULT,\
         SCAN, PRINT, ATOI, ATOB, ATOF, TO_STRING, TRUE, FALSE, PLUS, MINUS, MULT, DIV, MOD, \
-        COMMA, SEMICOLON, LBRACKET, RBRACKET, LBRACE, RBRACE, EQUAL, LESS, MORE, \
-        IDENTIFIER, NUM_INT, NUM_DOUBLE = range(37)
+        COMMA, SEMICOLON, LBRACKET, RBRACKET, LBRACE, RBRACE, EQUAL, LESS, MORE, AND, OR, NOT, \
+        IDENTIFIER, NUM_INT, NUM_DOUBLE = range(40)
+
+    SPECIAL_SYMBOLS = {
+        '+': PLUS,
+        '-': MINUS,
+        '*': MULT,
+        '/': DIV,
+        '%': MOD,
+        ',': COMMA,
+        ';': SEMICOLON,
+        '(': LBRACKET,
+        ')': RBRACKET,
+        '{': LBRACE,
+        '}': RBRACE,
+        '=': EQUAL,
+        '<': LESS,
+        '>': MORE,
+        '&': AND,
+        '|': OR,
+        '!': NOT
+    }
 
     class NoMoreTokens(Exception):
         pass
@@ -21,17 +41,37 @@ class Lexer:
             self._program_text = f.read()
 
         self._curr_symbol_index = 0
-        self._text_len = len(self._program_text)
+        self._text_len = len(self._program_text) - 1  # EOF in the end?
+
+    def get_curr_symbol(self) -> str:
+        return self._program_text[self._curr_symbol_index]
+
+    def next_symbol(self):
+        self._curr_symbol_index += 1
+
+    def program_finished(self) -> bool:
+        return self._curr_symbol_index >= self._text_len
 
     def get_next_token(self) -> Token:
-        raise Lexer.NoMoreTokens()
+        if self.program_finished():
+            raise Lexer.NoMoreTokens()
+
+        curr_sym = self.get_curr_symbol()
+
+        if curr_sym in Lexer.SPECIAL_SYMBOLS.keys():
+            next_tok = Lexer.Token(Lexer.SPECIAL_SYMBOLS[curr_sym], None)
+        else:
+            next_tok = Lexer.Token(Lexer.INT, curr_sym)
+
+        self.next_symbol()
+        return next_tok
 
     def split_program_into_tokens(self) -> List[Token]:
         ret = []
 
         while True:
             try:
-                ret += self.get_next_token()
+                ret.append(self.get_next_token())
             except Lexer.NoMoreTokens:
                 break
 
