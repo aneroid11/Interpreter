67f7ea42f2a034588c3b51f2ade04c7363af14fb
aneroid11 <a-e-v2003@yandex.by>
2023-03-21 00:17:37 +0300

    before starting the parser

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,6 +1,13 @@
 from lexer import Lexer
 
 
+def print_table(table):
+    sz = len(table)
+
+    for i in range(sz):
+        print(f"{i}: {table[i]}")
+
+
 class Compiler:
     def __init__(self, program_name: str):
         self._identifiers_table = []
@@ -18,7 +25,19 @@ class Compiler:
     def do_lexical_analysis(self):
         self._tokens_list = self._lexer.split_program_into_tokens()
 
-        print("TOKENS\n")
+        print("KEYWORDS")
+        print_table(self._keywords_table)
+
+        print("\nIDENTIFIERS")
+        print_table(self._identifiers_table)
+
+        print("\nCONSTANTS")
+        print_table(self._constants_table)
+
+        print("\nOPERATORS")
+        print_table(self._operators_table)
+
+        print("\nTOKENS LIST\n")
 
         for tok in self._tokens_list:
             if tok.table is self._operators_table:
@@ -31,6 +50,3 @@ class Compiler:
                 tp = "identifier"
 
             print(f"[{tp}, {tok.index_in_table}], ({tok.line}, {tok.index})", end="\n")
-            # print(f"[{tok.table}, {tok.index_in_table}]", end="\n")
-        # for tok in self.tokens_list:
-        #     print(f"[{Lexer.TYPES_OF_TOKENS[tok.type]}, {tok.value}] {tok.line, tok.index}", end="\n")
