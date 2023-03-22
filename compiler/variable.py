833667d11cac8a69ad3c6e0a51e25c8439bc0627
aneroid11 <a-e-v2003@yandex.by>
2023-03-22 16:39:23 +0300

    first version of the Variable class

---
+++ compiler/variable.py
@@ -0,0 +1,11 @@
+class Variable:
+    def __init__(self, name: str, tp: str):
+        self.name = name
+        self.type = tp
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
+
+    def __eq__(self, other):
+        return self.name == other.name
+        # return self.value == other.value and self.type == other.type
