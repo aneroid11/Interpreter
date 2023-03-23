5cab5454ae4c79e1806e0a70af277ae31fd2ee6d
aneroid11 <a-e-v2003@yandex.by>
2023-03-23 10:06:08 +0300

    remove unused symbol.py

--- compiler/symbol.py
+++
@@ -1,5 +0,0 @@
-class Symbol:
-    def __init__(self, identifier: str, scope: str, data_type=None):
-        self.identifier = identifier
-        self.scope = scope
-        self.data_type = data_type
