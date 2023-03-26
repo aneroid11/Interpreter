017d38dff8df38a0788a053426715aef89eec180
aneroid11 <a-e-v2003@yandex.by>
2023-03-26 16:34:27 +0300

    mod operands error

--- compiler/semantic.py
+++ compiler/semantic.py
@@ -19,6 +19,10 @@ class SemanticAnalyzer:
         def __init__(self, line: int, index: int):
             super().__init__("division by zero", line, index)
 
+    class InvalidModOperands(SemanticError):
+        def __init__(self, line: int, index: int):
+            super().__init__("operand of % is not int", line, index)
+
     def __init__(self, parser_nodes: list, operators: list, identifiers: list, keywords: list,
                  consts: list, syntax_tree):
         self._parser_nodes = parser_nodes
@@ -27,6 +31,19 @@ class SemanticAnalyzer:
         self._keywords = keywords
         self._consts = consts
         self._syntax_tree = syntax_tree
+
+    def _check_int_expression(self, root: Parser.Node):
+        if root is None:
+            return
+
+        if root.table is self._consts and root.value().type == Constant.DOUBLE:
+            raise SemanticAnalyzer.InvalidModOperands(root.line, root.index)
+        if root.table is self._keywords and root.value() == "atof":
+            raise SemanticAnalyzer.InvalidModOperands(root.line, root.index)
+        if root.table is self._keywords and root.value() == "atoi":
+            return
+        if root.table is self._identifiers and root.value().type == "double":
+            raise SemanticAnalyzer.InvalidModOperands(root.line, root.index)
 
     def _traverse_tree(self, root: Parser.Node):
         if root is None:
@@ -38,6 +55,10 @@ class SemanticAnalyzer:
             if divisor.table is self._consts and divisor.value().type in (Constant.DOUBLE, Constant.INT) \
                 and float(divisor.value().value) == 0:
                 raise SemanticAnalyzer.DivisionByZero(divisor.line, divisor.index)
+        elif root.table is self._operators and root.value() == "%":
+            self._check_int_expression(root.children[0])
+            self._check_int_expression(root.children[1])
+            return
 
         for child in root.children:
             self._traverse_tree(child)
