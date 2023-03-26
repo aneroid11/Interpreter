c1ff4ec7b2619ea578d81884edb95307033e3652
aneroid11 <a-e-v2003@yandex.by>
2023-03-26 16:03:59 +0300

    traversing the tree

--- compiler/semantic.py
+++ compiler/semantic.py
@@ -1,3 +1,6 @@
+from parser import Parser
+
+
 class SemanticAnalyzer:
     def __init__(self, parser_nodes: list, operators: list, identifiers: list, keywords: list,
                  consts: list, syntax_tree):
@@ -8,6 +11,16 @@ class SemanticAnalyzer:
         self._consts = consts
         self._syntax_tree = syntax_tree
 
+    def _traverse_tree(self, root: Parser.Node):
+        if root is None:
+            return
+
+        if root.table is self._keywords and root.value() == "for":
+            print("I found for!")
+
+        for child in root.children:
+            self._traverse_tree(child)
+
     def check_for_semantic_errors(self):
-        print()
-        print("no errors found")
+        print("\nSEMANTIC ANALYSIS")
+        self._traverse_tree(self._syntax_tree)
