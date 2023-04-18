2f0205568fd0bf486dbc5b2abf4dd0faab46f0a3
aneroid11 <a-e-v2003@yandex.by>
2023-04-18 14:21:26 +0300

    running hello world

--- compiler/interpreter.py
+++ compiler/interpreter.py
@@ -1,5 +1,6 @@
 from working_with_syntax_tree import WorkingWithSyntaxTree
 from parser import Parser
+from constant import Constant
 
 
 class Interpreter(WorkingWithSyntaxTree):
@@ -7,10 +8,20 @@ class Interpreter(WorkingWithSyntaxTree):
                  consts: list, syntax_tree):
         super().__init__(parser_nodes, operators, identifiers, keywords, consts, syntax_tree)
 
+    def _compute_string_expression(self, expr_node: Parser.Node) -> str:
+        if expr_node.table is self._consts_tbl and expr_node.value().type == Constant.STRING:
+            str_to_print = expr_node.value().value
+            return bytes(str_to_print, "utf-8").decode("unicode_escape")
+
+        return "unknown string"
+
+    def _run_print(self, print_node: Parser.Node):
+        print(self._compute_string_expression(print_node.children[0]), end='')
+
     def _run_program(self, prog_node: Parser.Node):
         for stmt_node in prog_node.children:
             if self._is_keyword(stmt_node, "print"):
-                print("execute print statement")
+                self._run_print(stmt_node)
 
     def run_program(self):
         self._run_program(self._syntax_tree)
