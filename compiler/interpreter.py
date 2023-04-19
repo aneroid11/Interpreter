68436582bbe6fa5b06f24d505c0793239244da08
aneroid11 <a-e-v2003@yandex.by>
2023-04-19 12:19:17 +0300

    arithmetic, boolean and string expressions

--- compiler/interpreter.py
+++ compiler/interpreter.py
@@ -12,48 +12,6 @@ class Interpreter(WorkingWithSyntaxTree):
         str_to_print = str_const_node.value().value
         return bytes(str_to_print, "utf-8").decode("unicode_escape")
 
-    def _interpret_scan(self) -> str:
-        return input()
-
-    def _compute_bool_expr(self, expr_node: Parser.Node) -> bool:
-        if self._is_keyword(expr_node, ("true", "false")):
-            return expr_node.value() == "true"
-        elif self._is_operator(expr_node, '||'):
-            return self._compute_bool_expr(expr_node.children[0]) or \
-                   self._compute_bool_expr(expr_node.children[1])
-        elif self._is_operator(expr_node, '&&'):
-            return self._compute_bool_expr(expr_node.children[0]) and \
-                   self._compute_bool_expr(expr_node.children[1])
-        elif self._is_operator(expr_node, '!'):
-            return not self._compute_bool_expr(expr_node.children[0])
-
-        return False
-
-    def _compute_bool_arithm_or_string_expr(self, expr_node: Parser.Node):
-        if self._is_operator(expr_node, ('||', '&&', '!', '>', '<', '>=', '<=', '==', '!=')) or \
-            self._is_keyword(expr_node, ("true", "false")):  # or \
-            # self._is_identifier_of_type(expr_node, "bool"):
-            return self._compute_bool_expr(expr_node)
-
-        return "BAS_expr"
-
-    def _interpret_to_string(self, to_str_node: Parser.Node) -> str:
-        expr_result = self._compute_bool_arithm_or_string_expr(to_str_node.children[0])
-        return str(expr_result)
-
-    def _compute_string_expression(self, expr_node: Parser.Node) -> str:
-        if self._is_string_constant(expr_node):
-            return self._compute_string_constant(expr_node)
-        elif self._is_operator(expr_node, '+'):
-            return (self._compute_string_expression(expr_node.children[0]) +
-                    self._compute_string_expression(expr_node.children[1]))
-        elif self._is_keyword(expr_node, "scan"):
-            return self._interpret_scan()
-        elif self._is_keyword(expr_node, "to_string"):
-            return self._interpret_to_string(expr_node)
-
-        return "unknown string"
-
     def _run_print(self, print_node: Parser.Node):
         print(self._interpret_node(print_node.children[0]), end='')
 
@@ -78,6 +36,36 @@ class Interpreter(WorkingWithSyntaxTree):
             if ret == "False":
                 return "false"
             return ret
+        elif self._is_keyword(node, "scan"):
+            return input()
+        elif self._is_operator(node, '+'):
+            return self._interpret_node(node.children[0]) + self._interpret_node(node.children[1])
+        elif self._is_operator(node, '-'):
+            return self._interpret_node(node.children[0]) - self._interpret_node(node.children[1])
+        elif self._is_operator(node, '*'):
+            return self._interpret_node(node.children[0]) * self._interpret_node(node.children[1])
+        elif self._is_operator(node, '/'):
+            return self._interpret_node(node.children[0]) / self._interpret_node(node.children[1])
+        elif self._is_operator(node, '%'):
+            return self._interpret_node(node.children[0]) % self._interpret_node(node.children[1])
+        elif self._is_operator(node, '&&'):
+            return self._interpret_node(node.children[0]) and self._interpret_node(node.children[1])
+        elif self._is_operator(node, '||'):
+            return self._interpret_node(node.children[0]) or self._interpret_node(node.children[1])
+        elif self._is_operator(node, '!'):
+            return not self._interpret_node(node.children[0])
+        elif self._is_operator(node, '>'):
+            return self._interpret_node(node.children[0]) > self._interpret_node(node.children[1])
+        elif self._is_operator(node, '<'):
+            return self._interpret_node(node.children[0]) < self._interpret_node(node.children[1])
+        elif self._is_operator(node, '>='):
+            return self._interpret_node(node.children[0]) >= self._interpret_node(node.children[1])
+        elif self._is_operator(node, '<='):
+            return self._interpret_node(node.children[0]) <= self._interpret_node(node.children[1])
+        elif self._is_operator(node, '=='):
+            return self._interpret_node(node.children[0]) == self._interpret_node(node.children[1])
+        elif self._is_operator(node, '!='):
+            return self._interpret_node(node.children[0]) != self._interpret_node(node.children[1])
         else:
             print(f"Runtime error: unknown node: {node.line}:{node.index}")
             exit(1)
