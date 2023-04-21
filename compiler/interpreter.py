56cc07d791c9c52ac73fabced349a02f5743546a
aneroid11 <a-e-v2003@yandex.by>
2023-04-21 12:25:39 +0300

    replace character in a string

--- compiler/interpreter.py
+++ compiler/interpreter.py
@@ -37,12 +37,37 @@ class Interpreter(WorkingWithSyntaxTree):
             self._idents_tbl[left_node.index_in_table].value = value
         else:
             arr_to_assign = self._idents_tbl[left_node.children[0].index_in_table].value
+            arr_elem_tp = self._idents_tbl[left_node.children[0].index_in_table].type[0]
+            num_indexes_in_decl = len(self._idents_tbl[left_node.children[0].index_in_table].type) - 1
+            index_nodes = left_node.children[1:]
 
             try:
-                for index_node in left_node.children[1:-1]:
-                    idx = self._interpret_node(index_node)
-                    arr_to_assign = arr_to_assign[idx]
-                arr_to_assign[self._interpret_node(left_node.children[len(left_node.children) - 1])] = value
+                if arr_elem_tp != "string" or len(index_nodes) == num_indexes_in_decl:
+                    for i in range(len(index_nodes)):
+                        idx = self._interpret_node(index_nodes[i])
+
+                        if i == len(index_nodes) - 1:
+                            arr_to_assign[idx] = value
+                        else:
+                            arr_to_assign = arr_to_assign[idx]
+                else:
+                    # this is a string
+
+                    for i in range(len(index_nodes) - 1):
+                        idx = self._interpret_node(index_nodes[i])
+
+                        if i == len(index_nodes) - 2:
+                            idx2 = self._interpret_node(index_nodes[i + 1])
+                            arr_to_assign[idx] = arr_to_assign[idx][:idx2] + value + arr_to_assign[idx][(idx2 + 1):]
+                        else:
+                            arr_to_assign = arr_to_assign[idx]
+
+                # for index_node in index_nodes:
+                #     idx = self._interpret_node(index_node)
+                #     arr_to_assign = arr_to_assign[idx]
+                #
+                # idx = self._interpret_node(left_node.children[len(left_node.children) - 1])
+                # arr_to_assign[idx] = value
             except IndexError:
                 raise Interpreter.RuntimeError("index out of range", left_node.line, left_node.index)
 
